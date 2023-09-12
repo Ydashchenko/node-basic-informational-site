5b0bb15e73a02fff2e2dad9e7bfbf4f9c1213942
madjonga <madjonga123@gmail.com>
2023-09-12 14:00:02 +0300

    Rewrite the program without using express

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  // Get the path from the URL
+  const { pathname } = parsedUrl;
+
+  // Map URLs to corresponding HTML files
+  let filePath = '.' + pathname + '.html';
+
+  // Handle the case when no path is provided (localhost:8080)
+  if (pathname === '/') {
+    filePath = './index.html';
+  }
+
+  // Check if the requested file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // File not found, serve the 404 page
+      fs.readFile('./404.html', (err, notFoundData) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/html' });
+          res.end('500 Internal Server Error');
+        } else {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(notFoundData);
+        }
+      });
+    } else {
+      // File exists, serve it
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/html' });
+          res.end('500 Internal Server Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    }
+  });
+});
+
 const port = 8080;
-
-// Serve static files from a directory named 'public'
-app.use(express.static('public'));
-
-// Define routes for each page
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/about.html');
-});
-
-app.get('/contact-me', (req, res) => {
-  res.sendFile(__dirname + '/contact-me.html');
-});
-
-// 404 - Page not found
-app.use((req, res) => {
-  res.status(404).sendFile(__dirname + '/404.html');
-});
-
-// Start the server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
